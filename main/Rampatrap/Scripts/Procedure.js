218fc82ef0ea6c784881f26151082195864e7d1f
Tobias Hölzl <t.hoelzl@students.htl-leonding.ac.at>
2026-01-21 12:33:38 +0100

    Procedure.js von Rampatrap angefangen mit erstem Szenen switch und Audio abspielen

--- main/Rampatrap/Scripts/Procedure.js
+++ main/Rampatrap/Scripts/Procedure.js
@@ -1,5 +1,15 @@
 villageStartScene();
 
-setTimeout(5000);
+const audio = new Audio('audios/rampatrapVillageStartScene.mp3');
+audio.play();
 
-houseScene();
+audio.addEventListener('ended', function() {
+    houseScene();
+});
+
+const audio2 = new Audio('audios/rampatrapHouseDecideScene.mp3');
+audio2.play();
+
+audio2.addEventListener('ended', function() {
+   villageStartScene();
+});
